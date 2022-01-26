243d41938644a2968bb2df8ed0a090c877d0e49c
Yazawazi <yazawazi@qq.com>
2022-01-26 20:33:08 +0800

    (未测试) 扩展解析器

--- sites/waziAsianSister.py
+++ sites/waziAsianSister.py
@@ -1,3 +1,4 @@
+from markupsafe import re
 from mods import waziFun
 from bs4 import BeautifulSoup
 from mods.waziURL import waziURL
@@ -54,8 +55,81 @@ class waziAsianSister:
         fuName = waziFun.getFuncName()
         waziLog.log("debug", f"({self.name}.{fuName}) 收到 Soup，正在解析。")
         person = {}
-
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取个人信息。")
+        person["name"] = soup.find("center").find("h1").text
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取个人描述。")
+        person["descriptionHTML"] = str(soup.find("div", {"class": "detailBox"})).replace("<br>", "\n")
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取个人浏览量。")
+        person["views"] = int(soup.find("div", {"class": "detailBox"}).find("div").text.split(": ")[1].split("标签")[0])
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取个人标签。")
+        person["tags"] = []
+        for i in soup.find("div", {"class": "detailBox"}).find("div").find("h4").find_all("a"):
+            if i.attrs["href"] == "tag.php?tag=":
+                pass
+            else:
+                waziLog.log("debug", f"({self.name}.{fuName}) 个人标签： {i.text}。")
+                person["tags"].append({"name": i.text, "link": i.attrs["href"]})
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取相关推荐。")
+        person["galleries"] = waziAsianSister.parseRecommendImagesAndVideos(self, soup)[0]
+        person["videos"] = waziAsianSister.parseRecommendImagesAndVideos(self, soup)[1]
+        waziLog.log("info", f"({self.name}.{fuName}) 解析完成，个人信息返回中。")
+        return person
     
+    def parseRecommendImagesAndVideos(self, soup):
+        fuName = waziFun.getFuncName()
+        waziLog.log("debug", f"({self.name}.{fuName}) 收到 Soup，正在解析。")
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取推荐画廊和视频。")
+        recommendGalleries = []
+        recommendVideos = []
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取推荐画廊。")
+        for i in soup.find_all("div", {"class": "recommentBox"}):
+            gallery = {}
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊地址。")
+            gallery["link"] = i.find("a").attrs["href"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊封面。")
+            gallery["cover"] = i.find("img", {"class": "lazyload"}).attrs["data-src"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊封面缺省字符串。")
+            gallery["alt"] = i.find("img", {"class": "lazyload"}).attrs["alt"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊标题。")
+            gallery["title"] = i.find("a").attrs["title"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊星级。")
+            gallery["stars"] = len(i.find_all("img", {"class": "recommentStar"}))
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在检查该画廊是否需要 VIP。")
+            if i.find("img", {"class": "rec_vip_cover"}):
+                gallery["VIP"] = True
+            else:
+                gallery["VIP"] = False
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在添加到画廊列表。")
+            recommendGalleries.append(gallery)
+            waziLog.log("debug", f"({self.name}.{fuName}) 添加完成。")
+        waziLog.log("debug", f"({self.name}.{fuName}) 正在获取推荐视频。")
+        for i in soup.find_all("div", {"class": "recommentBoxVideo"}):
+            video = {}
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频是否存在 data 标签。")
+            if i.attrs["data"]:
+                video["data"] = i.attrs["data"]
+            else:
+                video["data"] = ""
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频地址。")
+            video["link"] = i.find("a").attrs["href"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频标题。")
+            video["title"] = i.find("a").attrs["title"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频封面。")
+            if i.find("img", class_ = "lazyload").attrs["data-src"].startswith("http"):
+                video["cover"] = i.find("img", class_ = "lazyload").attrs["data-src"]
+            else:
+                video["cover"] = "https://asiansister.com/" + i.find("img", class_ = "lazyload").attrs["data-src"]
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在检查该视频是否需要 VIP。")
+            if i.find("img", {"class": "rec_vip_cover"}):
+                video["VIP"] = True
+            else:
+                video["VIP"] = False
+            waziLog.log("debug", f"({self.name}.{fuName}) 正在添加到视频列表。")
+            recommendVideos.append(video)
+            waziLog.log("debug", f"({self.name}.{fuName}) 添加完成。")
+        waziLog.log("info", f"({self.name}.{fuName}) 提取页面中所有信息完成，图像信息： {recommendGalleries}, 视频信息： {recommendVideos}。")
+        return recommendGalleries, recommendVideos
+
     def parseImagesAndVideos(self, soup):
         fuName = waziFun.getFuncName()
         waziLog.log("debug", f"({self.name}.{fuName}) 收到 Soup，正在解析。")
@@ -68,26 +142,19 @@ class waziAsianSister:
             gallery = {}
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取浏览数量。")
             gallery["views"] = int(i.find("div", class_ = "viewCountBox").text)
-            waziLog.log("debug", f"({self.name}.{fuName}) 浏览数量为： {gallery['views']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊链接。")
             gallery["link"] = "https://asiansister.com/" + i.find("a")["href"]
-            waziLog.log("debug", f"({self.name}.{fuName}) 画廊链接为： {gallery['link']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在检查该画廊是否需要 VIP。")
             if i.find("img", class_ = "vip_cover"):
-                waziLog.log("debug", f"({self.name}.{fuName}) 该画廊需要 VIP 权限。")
                 gallery["vip"] = True
             else:
-                waziLog.log("debug", f"({self.name}.{fuName}) 该画廊不需要 VIP 权限。")
                 gallery["vip"] = False
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊封面。")
             gallery["cover"] = "https://asiansister.com/" + i.find("img", class_ = "lazyload").attrs["data-src"]
-            waziLog.log("debug", f"({self.name}.{fuName}) 画廊封面为： {gallery['cover']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊封面的顶替字符串。")
             gallery["alt"] = i.find("img", class_ = "lazyload").attrs["alt"]
-            waziLog.log("debug", f"({self.name}.{fuName}) 画廊封面的顶替字符串为： {gallery['alt']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取画廊标题。")
             gallery["title"] = i.find("div", class_ = "titleName").text.strip()
-            waziLog.log("debug", f"({self.name}.{fuName}) 画廊标题为： {gallery['title']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在添加到画廊列表。")
             galleriesBox.append(gallery)
             waziLog.log("debug", f"({self.name}.{fuName}) 添加完成。")
@@ -97,33 +164,25 @@ class waziAsianSister:
             video = {}
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频是否存在 data 标签。")
             if i.attrs["data"]:
-                waziLog.log("debug", f"({self.name}.{fuName}) 视频存在 data 标签。")
                 video["data"] = i.attrs["data"]
             else:
-                waziLog.log("debug", f"({self.name}.{fuName}) 视频不存在 data 标签。")
                 video["data"] = None
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频播放量。")
             video["views"] = int(i.find("div", class_ = "viewCountBox").text)
-            waziLog.log("debug", f"({self.name}.{fuName}) 视频播放量为： {video['views']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频链接。")
             video["link"] = "https://asiansister.com/" + i.find("a")["href"]
-            waziLog.log("debug", f"({self.name}.{fuName}) 视频链接为： {video['link']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在检查该视频是否需要 VIP。")
             if i.find("img", class_ = "vip_cover"):
-                waziLog.log("debug", f"({self.name}.{fuName}) 该视频需要 VIP 权限。")
                 video["vip"] = True
             else:
-                waziLog.log("debug", f"({self.name}.{fuName}) 该视频不需要 VIP 权限。")
                 video["vip"] = False
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频封面。")
             if i.find("img", class_ = "lazyload").attrs["data-src"].startswith("http"):
                 video["cover"] = i.find("img", class_ = "lazyload").attrs["data-src"]
             else:
                 video["cover"] = "https://asiansister.com/" + i.find("img", class_ = "lazyload").attrs["data-src"]
-            waziLog.log("debug", f"({self.name}.{fuName}) 视频封面为： {video['cover']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在获取视频标题。")
             video["title"] = i.find("div", class_ = "titleName_video").text.strip()
-            waziLog.log("debug", f"({self.name}.{fuName}) 视频标题为： {video['title']}。")
             waziLog.log("debug", f"({self.name}.{fuName}) 正在添加到视频列表。")
             videosBox.append(video)
             waziLog.log("debug", f"({self.name}.{fuName}) 添加完成。")
@@ -154,7 +213,7 @@ class waziAsianSister:
         soup = waziAsianSister.returnSoup(self, url)
         return waziAsianSister.parseImagesAndVideos(self, soup)
 
-    def customSearch(self, content, type):
+    def customParse(self, content, type):
         fuName = waziFun.getFuncName()
         waziLog.log("debug", f"({self.name}.{fuName}) 收到补充 URL 和类型，正在生成 URL： {content}， {type}。")
         url = "https://asiansister.com/" + content
