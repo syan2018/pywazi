47d0746e239f0ecbf654b0031645a100e10a8b15
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-02-10 08:37:41 +0800

    占位

--- sites/waziNyaa.py
+++ sites/waziNyaa.py
@@ -1,5 +1,36 @@
+from mods import waziFun
+from ins.waziInsLog import waziLog
+
 class waziNyaa:
     # TODO: Support sukebei.nyaa.si and nyaa.si
     def __init__(self):
         super(waziNyaa, self).__init__()
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/91.0.4472.164 Safari/537.36"
+        }
+        self.proxies = {
+            "proxyAddress": "127.0.0.1",
+            "proxyPort": "7890"
+        }
+        self.params = {}
         self.name = self.__class__.__name__
+    
+    def giveParams(self, params):
+        fuName = waziFun.getFuncName()
+        waziLog.log("debug", f"({self.name}.{fuName}) 收到配置信息，正在写入。")
+        self.params = params
+        waziLog.log("info", f"({self.name}.{fuName}) 写入完成，目前配置为： {self.params}")
+        return self.params
+
+    def parsePage(self, soup):
+        pass
+
+    def parseRSS(self, rss):
+        pass
+
+    def search(self, params):
+        pass
+
+    def searchRSS(self, params):
+        pass
