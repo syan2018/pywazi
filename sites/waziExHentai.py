8237532e6c1b8e0df9e2ef3ca34b0374dbbaae30
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-02-15 15:32:02 +0800

    怪

--- sites/waziExHentai.py
+++ sites/waziExHentai.py
@@ -86,11 +86,8 @@ class waziExHentai:
         """
         super(waziExHentai, self).__init__()
         self.headers = {
-            "Host": "exhentai.org",
             "Connection": "keep-alive",
-            "Cache-Control": "max-age=0",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.3",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "Cookie": ""
         }
         self.proxies = {
@@ -2507,7 +2504,6 @@ class waziExHentai:
         soup = waziExHentai.returnSoup(self, mpvUrl)
         waziLog.log("debug", f"({self.name}.{fuName}) 页面信息获取完成。")
         waziLog.log("debug", f"({self.name}.{fuName}) 正在获取页面中的 JavaScript 脚本。")
-        print(str(soup))
         scripts = str(soup.find_all("script")[1]).split("<script type=\"text/javascript\">")[1].split("</script>")[0]
         waziLog.log("debug", f"({self.name}.{fuName}) JS 脚本获取完成： {scripts}")
         waziLog.log("debug", f"({self.name}.{fuName}) 正在获取 Image List 数据。")
