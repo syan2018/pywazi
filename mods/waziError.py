6ac23bf435105b1097c37c2476fe0b83f6771478
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-02-20 18:21:55 +0800

    只是帮助我进行网络查阅的debug

---
+++ mods/waziError.py
@@ -0,0 +1,67 @@
+"""
+mods/waziError.py
+
+class: waziError
+"""
+
+import traceback
+import webbrowser
+
+class waziDebug:
+    """
+    waziDebug
+    *Debug with internet. Lazy, lazy.*
+
+    Use stackoverflow to debug.
+
+    Attributes:
+        (private) _function: function
+            The function to debug.
+
+    Methods:
+        - Please use help()
+    """
+    def __init__(self, function):
+        """
+        waziDebug.__init__(self, function)
+        *May useful.*
+
+        Initialize the class.
+
+        Parameters:
+            function: function
+                The function to debug.
+        """
+        self._function = function
+    
+    def __call__(self, *args, **kwargs):
+        """
+        waziError.__call__(self, *args, **kwargs)
+        *Iris.*
+
+        Debug the function.
+
+        Parameters:
+            *args: list
+                The args of the function.
+            
+            **kwargs: dict
+                The kwargs of the function.
+
+        Return:
+            Function return
+            If error, will open the stackoverflow and exit the program.
+
+        Errors:
+            None
+        """
+        try:
+            info = self._function(*args, **kwargs)
+        except Exception as e:
+            error = traceback.format_exc()
+            url = "https://stackoverflow.com/search?q=%5Bpython%5D+" + error.split("\n")[-2]
+            webbrowser.open(url)
+            traceback.print_exc()
+            exit()
+        else:
+            return info
